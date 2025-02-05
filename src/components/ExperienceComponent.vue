49e37ca29da0fbdc84253042c5c462a59b164b35
Justin Angrendo <jluvian25@gmail.com>
2025-02-05 16:19:32 +0700

    Experience Section

---
+++ src/components/ExperienceComponent.vue
@@ -0,0 +1,135 @@
+<template>
+  <section class="experience" id="experience">
+    <div class="eTitle">
+      <h1>EXPERIENCES</h1>
+    </div>
+    <div class="timeline">
+      <div class="line"></div>
+      <div v-for="(exp, index) in experiences" :key="index" class="section">
+        <div class="bead"></div>
+        <div 
+          class="experience-item" 
+          :class="{'left': index % 2 === 0, 'right': index % 2 !== 0}"
+        >
+          <div class="exp">
+            <h2>{{ exp.title }}</h2>
+            <h3>{{ exp.date }}</h3>
+            <p v-html="exp.description"></p>
+            <img v-for="(img, imgIndex) in exp.images" :key="imgIndex" :src="img" alt="Experience Image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script src="./js/experience-component.js"></script>
+
+<style scoped>
+.experience {
+  width: 100%;
+}
+
+.eTitle {
+  text-align: center;
+  color: var(--primarycolor);
+  font-size: 32px;
+  position: relative;
+}
+
+.eTitle h1 {
+  font-size: 48px;
+}
+
+.eTitle h1::after {
+  content: "";
+  position: absolute;
+  bottom: -10px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 78vw;
+  height: 2px;
+  background-color: var(--primarycolor);
+}
+
+.timeline {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 48px;
+}
+
+.line {
+  position: absolute;
+  z-index: 1;
+  left: 50%;
+  width: 2px;
+  height: 100%;
+  background-color: var(--primarycolor);
+  transform: translateX(-50%);
+}
+
+.section {
+  display: flex;
+  position: relative;
+  z-index: 2;
+  align-items: center;
+  margin-bottom: 16px;
+  width: 60%;
+}
+
+.bead {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  background-color: var(--primarycolor);
+  transform: translate(-50%, -50%);
+}
+
+.experience-item {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  padding: 16px 48px;
+  border-radius: 10px;
+  color: var(--primarycolor);
+  text-align: justify;
+}
+
+.experience-item.left {
+  transform: translateX(-25%); 
+  margin-left: 32px;
+}
+
+.experience-item.right {
+  transform: translateX(75%); 
+}
+
+.exp {
+  flex-grow: 1;
+  padding-left: 36px;
+}
+
+.exp h2 {
+  margin-bottom: 8px;
+}
+
+.exp h3 {
+  margin-bottom: 16px;
+}
+
+.exp p {
+  font-size: 16px;
+  font-weight: 300;
+}
+
+.section img {
+  margin-top: 16px;
+  width: 100%;
+  max-width: 400px;
+}
+</style>
---
+++ src/components/js/experience-component.js
@@ -0,0 +1,60 @@
+import { ref } from 'vue';
+
+import staffLntImg from '@/assets/img/staff-lnt.png';
+import aktivisImg from '@/assets/img/aktivis.png';
+import uncoverYourSkillImg from '@/assets/img/uncover-your-skill.png';
+import BNCCUncoverYourSkillImg from '@/assets/img/bncc-uncover-your-skill.png';
+import certiciateMentorshipImg from '@/assets/img/certificate-scholarship-mentor.png';
+import blibliIntern1 from '@/assets/img/blibli-intern-1.jpg';
+import blibliIntern2 from '@/assets/img/blibli-intern-2.jpg';
+
+export default {
+  name: 'ExperienceComponent',
+  setup () {
+    const experiences = ref([
+      {
+        title: 'BNCC Activist',
+        date: 'Oct 2021 - Sep 2022',
+        description: `Serving as a member of the Learning and Training Division (LnT) while enhancing and cultivating an organizational mindset. Engage in the Leadership Development Program to further develop both soft and technical skills in the context of BNCC's organizational environment.`,
+        images: [aktivisImg]
+      },
+      {
+        title: 'BNCC LnT Staff',
+        date: 'Oct 2022 - Sep 2023',
+        description: `Responsible for managing BNCC Learning and Training Course.PIC of LnT C Programming Course. Train and Mentor BNCC LnT Activist. <br> Mentoring BNCC Activist in Leadership Development Program.`,
+        images: [staffLntImg]
+      },
+      {
+        title: 'PIC Mentor & Operator BNCC Uncover Your Skill',
+        date: 'April 2023 - May 2023',
+        description: `Creating Learning Modules of Data Science. Supporting participants throughout the workshop in using tools specific to the workshop's content.
+        <br>Assisting speakers in operating screens or handling technical aspects in the classroom.
+        <br>Setting up the workshop room.`,
+        images: [BNCCUncoverYourSkillImg, uncoverYourSkillImg]
+      },
+      {
+        title: 'Scholarship Mentor Bina Nusantara University',
+        date: 'Feb 2024 - Feb 2025',
+        description: `Mentored 3 Computer Science students in subjects like Compilation Techniques, Operating Systems, Algorithms and Programming, and Object-Oriented Programming, helping them improve their academic performance and build both technical skills and confidence.`,
+        images: [certiciateMentorshipImg]
+      },
+      {
+        title: 'Frontend Developer Intern at Blibli.com',
+        date: 'September 2023 - Jan 2024',
+        description: `<ul>
+        <li>Developed user interfaces (UI) using Vue.js, focusing on HTML structure, CSS styling, and JavaScript functionality.</li>
+        <li>Identified and resolved bugs and errors during development, ensuring seamless application performance.</li>
+        <li>Conducted unit testing to guarantee code quality and optimal functionality.</li>
+        <li>Led the migration of applications from Vue 2 to Vue 3, ensuring compatibility with the latest technologies.</li>
+        <li>Collaborated closely with the Quality Assurance (QA) team to test and validate applications prior to deployment.</li>
+        <li>Worked with the Back-End team to integrate APIs and ensure seamless data flow between front-end and back-end systems.</li>
+        <li>Documented development processes, including code changes, test cases, and implemented solutions.</li></ul>`,
+        images: [blibliIntern1, blibliIntern2]
+      }
+    ]);
+
+    return {
+      experiences
+    };
+  }
+};
